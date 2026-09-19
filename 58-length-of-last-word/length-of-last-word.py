1e4602239fdcec1098c068c2b6333af643560863
Manasi <patilmanasi735@gmail.com>
2026-09-19 13:49:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (8.73%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        length = 0
+        for i in range(len(s) -1,-1,-1):
+            if s[i] == " ":
+                if length > 0:
+                    return length
+            else:
+                length +=1
+        return length
+
+
+        
